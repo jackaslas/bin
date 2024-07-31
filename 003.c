c1ccc59b302f6c2d34ff30af07bf1b0303c8f82c
jackaslas <156167331+jackaslas@users.noreply.github.com>
2024-07-31 12:46:34 +0530

    Update 003.c

--- 003.c
+++ 003.c
@@ -1,37 +1,46 @@
 #include<stdio.h>
-int ne=1,min_cost=0;
-void main()
+int min(int a,int b)
 {
-    int n,i,j,min,a,u,b,v,cost[20][20],parent[20]; 
-    printf("Enter the no. of vertices:");
-    scanf("%d",&n); 
-    printf("\nEnter the cost matrix:\n");
-    for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++)
-            scanf("%d",&cost[i][j]);
-    for(i=1;i<=n;i++) 
-        parent[i]=0; 
-    printf("\nThe edges of spanning tree are\n");
-    while(ne<n)
-    {
-        min=999;
-        for(i=1;i<=n;i++)
-            for(j=1;j<=n;j++)
-                if(cost[i][j]<min){
-                    min=cost[i][j];
-                    a=u=i; 
-                    b=v=j;
-                }
-        while(parent[u])
-            u=parent[u];
-        while(parent[v])
-            v=parent[v];
-        if(u!=v){ 
-            printf("Edge %d\t(%d->%d)=%d\n",ne++,a,b,min);
-            min_cost=min_cost+min; 
-            parent[v]=u;
-        }
-        cost[a][b]=cost[a][b]=999;
-    }
-    printf("\nMinimum cost=%d\n",min_cost);
+if(a<b)
+return a;
+else
+return b;
+}
+void floyd(int dist[10][10],int n)
+{
+int i,j,k;
+for(k=1;k<=n;k++)
+{
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+}
+}
+}
+}
+int main()
+{
+int dist[10][10],n,i,j;
+printf("\nEnter the number of vertices:\n");
+scanf("%d",&n);
+printf("\nEnter the cost adjaceny matrix:\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+scanf("%d",&dist[i][j]);
+}
+}
+floyd(dist,n);
+printf("\nAll Pair Shortest path matrix is:\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+printf("%d\t",dist[i][j]);
+}
+printf("\n");
+}
 }
