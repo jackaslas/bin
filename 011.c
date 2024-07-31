2924c51b8faacb8e00e97c7f3db70c519fb76833
jackaslas <156167331+jackaslas@users.noreply.github.com>
2024-07-31 11:59:49 +0530

    Update 011.c

--- 011.c
+++ 011.c
@@ -1,90 +1,111 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<time.h>
-void mergesort(int a[100],int low,int high);
-void display(int a[100],int n);
+#include<stdlib.h>
+int a[60000],c[60000];
+void merge(int low,int mid,int high)
+{
+int i,j,k;
+k=low;
+i=low;
+j=mid+1;
+while(i<=mid && j<=high)
+{
+if(a[i]<a[j])
+c[k++]=a[i++];
+else
+c[k++]=a[j++];
+}
+while(i<=mid)
+c[k++]=a[i++];
+while(j<=high)
+c[k++]=a[j++];
+for(i=low;i<=k-1;i++)
+a[i]=c[i];
+}
+void merge_sort(int low,int high)
+{
+int mid;
+if(low<high)
+{
+mid=(low+high)/2;
+merge_sort(low,mid);
+merge_sort(mid+1,high);
+merge(low,mid,high);
+}
+}
 int main()
 {
- int i,low,high,n;
- int a[100],j,k;
- double time_required;
- clock_t begin,end;
-
- printf("\nEnter the number of elements :");
- scanf("%d",&n);
-
- for(i=0;i<n;i++)
-    a[i]=rand()%1000;
- low=0;
- high=n-1;
-  printf("\nBefore Sorting\n");
+int n,i;
+double total;
+clock_t start,end;
+printf("\nEnter the size of an array\n");
+scanf("%d",&n);
 for(i=0;i<n;i++)
-	printf("%d\t",a[i]);
- begin=clock();
- for(j=0;j<700;j++)
- for(k=0;k<700;k++)
- mergesort(a,low,high);
- end=clock();
- display(a,n);
- time_required=((double)(end-begin))/1000;
- printf("\nThe time required is: %f",time_required);
-
- return 0;
-}
-void mergesort(int a[10],int low,int high)
+a[i]=rand()%1000;
+printf("\n Array before sorting\n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+start=clock();
+merge_sort(0,n-1);
+end=clock();
+printf("\n Array after sorting\n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+total=(double)(end-start)/CLOCKS_PER_SEC;
+printf("\nThe total time taken is %f\n",total);
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int a[60000],c[60000];
+void merge(int low,int mid,int high)
 {
- int mid;
- void combine(int a[10],int low,int mid,int high);
- if(low<high)
- {
-  mid=(low+high)/2;
-  mergesort(a,low,mid);
-  mergesort(a,mid+1,high);
-  combine(a,low,mid,high);
- }
-}
-void combine(int a[10],int low,int mid,int high)
+int i,j,k;
+k=low;
+i=low;
+j=mid+1;
+while(i<=mid && j<=high)
 {
- int i,j,k;
- int temp[10];
- k=low;
- i=low;
- j=mid+1;
- while(i<=mid && j<=high)
- {
-  if(a[i]<=a[j])
-  {
-   temp[k]=a[i];
-   i++;
-   k++;
-  }
-  else
-  {
-   temp[k]=a[j];
-   j++;
-   k++;
-  }
- }
- while(i<=mid)
- {
-  temp[k]=a[i];
-  i++;
-  k++;
- }
- while(j<=high)
- {
-  temp[k]=a[j];
-  j++;
-  k++;
- }
- for(k=low;k<=high;k++)
- a[k]=temp[k];
+if(a[i]<a[j])
+c[k++]=a[i++];
+else
+c[k++]=a[j++];
 }
-void display(int a[10],int n)
+while(i<=mid)
+c[k++]=a[i++];
+while(j<=high)
+c[k++]=a[j++];
+for(i=low;i<=k-1;i++)
+a[i]=c[i];
+}
+void merge_sort(int low,int high)
 {
- int i;
- printf("\n\nThe sorted list is...\n");
- for(i=0;i<n;i++)
- printf("%d\t",a[i]);
+int mid;
+if(low<high)
+{
+mid=(low+high)/2;
+merge_sort(low,mid);
+merge_sort(mid+1,high);
+merge(low,mid,high);
 }
-
+}
+int main()
+{
+int n,i;
+double total;
+clock_t start,end;
+printf("\nEnter the size of an array\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+a[i]=rand()%1000;
+printf("\n Array before sorting\n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+start=clock();
+merge_sort(0,n-1);
+end=clock();
+printf("\n Array after sorting\n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+total=(double)(end-start)/CLOCKS_PER_SEC;
+printf("\nThe total time taken is %f\n",total);
+}
