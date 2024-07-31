523ed6deb7b7ed62932ccb8833eb245ee271f5e0
jackaslas <156167331+jackaslas@users.noreply.github.com>
2024-07-31 12:58:46 +0530

    Update 004.c

--- 004.c
+++ 004.c
@@ -1,33 +1,57 @@
 #include<stdio.h>
-int a,b,u,v,n,i,j,ne=1; 
-int visited[10]={0},min,mincost=0,cost[10][10];
-void main(){ 
-  printf("\n Enter the number of nodes:");
-  scanf("%d",&n); 
-  printf("\n Enter the adjacency matrix:\n");
-  for(i=1;i<=n;i++)
-  for(j=1;j<=n;j++){
-    scanf("%d",&cost[i][j]);
-    if(cost[i][j]==0)
-      cost[i][j]=999;
-  }
-  visited[1]=1; 
-  printf("\n");
-  while(ne<n){
-    for(i=1,min=999;i<=n;i++)
-      for(j=1;j<=n;j++)
-        if(cost[i][j]<min)
-          if(visited[i]!=0){
-            min=cost[i][j];
-            a=u=i; 
-            b=v=j;
-          }
-    if(visited[u]==0 || visited[v]==0){
-      printf("\n Edge %d:(%d %d) cost:%d",ne++,a,b,min);
-      mincost+=min;
-      visited[b]=1;
-    }
-    cost[a][b]=cost[b][a]=999;
-  }
-  printf("\n Minimun cost=%d",mincost);
+void dijk(int,int [20][20],int [20],int [20],int);
+int main()
+{
+int i,j,n,visited[20],source,cost[20][20],d[20];
+printf("Enter no. of vertices: ");
+scanf("%d",&n);
+printf("Enter the cost adjacency matrix\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+scanf("%d",&cost[i][j]);
+}
+}
+printf("\nEnter the source node: ");
+scanf("%d",&source);
+dijk(source,cost,visited,d,n);
+for(i=1;i<=n;i++)
+if(i!=source)
+printf("\nShortest path from %d to %d is %d",source,i,d[i]);
+}
+void dijk(int source,int cost[20][20],int visited[20],int d[20],int n)
+{
+int i,j,min,u,w;
+for(i=1;i<=n;i++)
+{
+visited[i]=0;
+d[i]=cost[source][i];
+}
+visited[source]=1;
+d[source]=0;
+for(j=2;j<=n;j++)
+{
+min=999;
+for(i=1;i<=n;i++)
+{
+if(!visited[i])
+{
+if(d[i]<min)
+{
+min=d[i];
+u=i;
+}
+}
+}
+visited[u]=1;
+for(w=1;w<=n;w++)
+{
+if(cost[u][w]!=999 && visited[w]==0)
+{
+if(d[w]>cost[u][w]+d[u])
+d[w]=cost[u][w]+d[u];
+}
+} 
+} 
 }
